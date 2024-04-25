ba32cae567943d7bfdbd8cea65b861e054ce1b77
IgorLima <igorlimadrum@gmail.com>
2024-04-24 23:33:13 -0300

    feat: create Header component

--- csr/src/App.js
+++ csr/src/App.js
@@ -1,9 +1,11 @@
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import TitleCategory from "./components/TitleCategory";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <TitleCategory category="Test" />
       <Footer />
     </div>

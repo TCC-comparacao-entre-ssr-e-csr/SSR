825efdbdd840260a206896770f725d89e63b59db
IgorLima <igorlimadrum@gmail.com>
2024-04-24 23:24:27 -0300

    feat: create Footer component

--- csr/src/App.js
+++ csr/src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import TitleCategory from "./components/TitleCategory";
 
 function App() {
   return (
     <div className="App">
       <TitleCategory category="Test" />
+      <Footer />
     </div>
   );
 }
